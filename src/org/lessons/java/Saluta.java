ec72c422b92929398ebded8a413ae872235598b4
1Pietro7D <103258899+1Pietro7D@users.noreply.github.com>
2023-01-24 15:50:22 +0100

    Add files via upload
    initial first String variable

--- src/org/lessons/java/Saluta.java
+++ src/org/lessons/java/Saluta.java
@@ -1,7 +1,9 @@
 package org.lessons.java;
 
 public class Saluta {
+	static String saluto = "Hello Java";
 	public static void main(String[] args) {
-		System.out.println("Hello Java"); // hello Java
+		System.out.println(saluto); // hello Java
 	}
 }
+ 
