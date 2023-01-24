895d6757ea5885e2d1b7ee9d2593c8ee83b36361
1Pietro7D <103258899+1Pietro7D@users.noreply.github.com>
2023-01-24 15:43:45 +0100

    real first file with only print out

--- src/org/lessons/java/Saluta.java
+++ src/org/lessons/java/Saluta.java
@@ -1,22 +1,7 @@
 package org.lessons.java;
+
 public class Saluta {
-    public static final int z = 3;
-   
-
-
-    public static void main(String[] args) {
-        System.out.println("Hello Java"); //hello Java
-        System.out.println(z);
-        
-        // VARIABLES
-
-        /////  STRING 
-        //  https://www.w3schools.com/java/java_ref_string.asp  (38)
-        //  String - stores text, such as "Hello". String values are surrounded by double quotes
-
-        final String name = "John";  // final as const
-        String res = "Result: ";
-        System.out.println(res.concat(name));
- }
+	public static void main(String[] args) {
+		System.out.println("Hello Java"); // hello Java
+	}
 }
- 
