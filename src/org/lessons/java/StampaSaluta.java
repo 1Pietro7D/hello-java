0359e3640c0cffa89d0d212ace2f9ffc873e9f5e
PietroDanieli <1pie7d@gmail.com>
2023-01-25 00:16:03 +0100

    Capitalize comment

--- src/org/lessons/java/StampaSaluta.java
+++ src/org/lessons/java/StampaSaluta.java
@@ -4,6 +4,6 @@ public class StampaSaluta {
 	static String saluto = "Hello Java";
 
 	public static void main(String[] args) {
-		System.out.println(saluto); // hello Java
+		System.out.println(saluto); // Hello Java
 	}
 }
