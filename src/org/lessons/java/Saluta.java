ff7c7f822952da5f1fddef41b88878b19cd00a0e
1Pietro7D <103258899+1Pietro7D@users.noreply.github.com>
2023-01-24 15:59:05 +0100

    header corrected
    with ctrl + shift +f

--- src/org/lessons/java/Saluta.java
+++ src/org/lessons/java/Saluta.java
@@ -2,8 +2,8 @@ package org.lessons.java;
 
 public class Saluta {
 	static String saluto = "Hello Java";
+
 	public static void main(String[] args) {
 		System.out.println(saluto); // hello Java
 	}
 }
- 
