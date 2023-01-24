e362eb9d6546c2a2d4d10303bb7ae391fd83a412
1Pietro7D <103258899+1Pietro7D@users.noreply.github.com>
2023-01-24 16:23:48 +0100

    Delete Saluta.java

--- src/org/lessons/java/Saluta.java
+++
@@ -1,9 +0,0 @@
-package org.lessons.java;
-
-public class Saluta {
-	static String saluto = "Hello Java";
-
-	public static void main(String[] args) {
-		System.out.println(saluto); // hello Java
-	}
-}
